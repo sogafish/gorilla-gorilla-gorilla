4b225fde8f02cdab08a7a2bc95846c6f63685565
sogafish <k.sogafish@gmail.com>
2020-08-19 08:38:56 +0900

    :package: GORM

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	// "gorilla-gorilla-gorilla/models"
+	"log"
+)
+
+func main() {
+	log.Println("Run")
+	// var userPost = models.GetPost()
+	// var userPosts = models.GetPosts()
+}
